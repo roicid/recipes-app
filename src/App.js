27d22e3cf0dbf14ef445a7d98ba03d969ff0a8bc
roicid <roi_cid89@hotmail.com>
2021-03-04 12:38:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, {useEffect , useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
+const App = () =>  {
+
+  const API_ID = "4c8500c5";
+  const API_KEY= "7fa1d51ac469fba0607a8e675225c308";
+  //const exampleRequest =  `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  
+  const [recipes , setRecipes]  = useState([]);
+
+  const [search , setSearch] = useState("") ; 
+
+  const [query , setQuery] = useState("chicken");
+
+
+
+  useEffect(() => {
+  
+    getRecipes();
+    } , [query]);  
+
+    const getRecipes = async () => {
+      const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+      const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+    }; 
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        
+        ;
+    };
+
+    const getSearch = e => {
+      e.preventDefault() ;
+      setQuery(search);
+      setSearch('') ; // esto hace q luego de apretar buscar , se ponga en blanco el buscador;
+    }
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit= {getSearch} className="search-form" >
+        <input className="search-bar" type= "text" value={search} onChange={updateSearch} />
+        <button className="search-buttom" type="submit">  search </button>
+        </form>
+<div className= "recipes">
+        {recipes.map(recipe=> (
+             
+             <Recipe  
+               key= {recipe.recipe.label}
+               title = {recipe.recipe.label}
+               calories= {recipe.recipe.calories}
+               image = {recipe.recipe.image}
+               ingredients = {recipe.recipe.ingredients}
+             />
+
+        ))}
+
+        </div>
+
+       
     </div>
   );
 }
